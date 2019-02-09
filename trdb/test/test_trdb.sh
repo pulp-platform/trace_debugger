fc304797785a1a8c8e28e74872041d9a8fb4d1d3
bluew <bluewww@users.noreply.github.com>
2019-02-09 20:49:02 +0100

    Fix path in test scripts

--- trdb/test/test_trdb.sh
+++ trdb/test/test_trdb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-../trdb --full-address --binary-format pulp --compress -o my_packets ../data/trdb_stimuli
-../trdb --full-address --binary-format pulp --dump my_packets
-../trdb --full-address --binary-format pulp --bfd ../data/interrupt --extract my_packets
-../trdb --full-address -dSsl --binary-format pulp --bfd ../data/interrupt --extract my_packets
+../main/trdb --full-address --binary-format pulp --compress -o my_packets ../data/trdb_stimuli
+../main/trdb --full-address --binary-format pulp --dump my_packets
+../main/trdb --full-address --binary-format pulp --bfd ../data/interrupt --extract my_packets
+../main/trdb --full-address -dSsl --binary-format pulp --bfd ../data/interrupt --extract my_packets
 rm my_packets
--- trdb/test/test_trdb_diff.sh
+++ trdb/test/test_trdb_diff.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-../trdb --binary-format pulp --compress -o my_packets ../data/trdb_stimuli
-../trdb --binary-format pulp --dump my_packets
-../trdb --binary-format pulp --bfd ../data/interrupt --extract my_packets
-../trdb -dSsl --binary-format pulp --bfd ../data/interrupt --extract my_packets
+../main/trdb --binary-format pulp --compress -o my_packets ../data/trdb_stimuli
+../main/trdb --binary-format pulp --dump my_packets
+../main/trdb --binary-format pulp --bfd ../data/interrupt --extract my_packets
+../main/trdb -dSsl --binary-format pulp --bfd ../data/interrupt --extract my_packets
 rm my_packets
